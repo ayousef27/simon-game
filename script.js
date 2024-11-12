545a9ad92061ec2fe5f385d53d1986fce280057c
ayousef27 <ahmenero27@gmail.com>
2024-11-12 15:25:09 +0300

    simon

---
+++ script.js
@@ -0,0 +1,35 @@
+// @ Variables
+
+// 1 sequence [] : to store sequence of squares (each square is presented by an index(0 - 8))
+
+// 2 playerInput [] : to store players input sequence
+
+// 3 gameOver : true or false to indicate if game is over or not
+
+// 4 currentLevel : to track current level (starting from 1)
+
+// 5 squareColors [] : to store colors of each square (9 colors)
+
+// 6 playerScore : to track player score (each level is 1 point)
+
+// 7 gridSize : a number for grid (3 X 3) 3 rows and 3 columns
+
+// 8 numSquares : to track numbers of squares (9 in this case)
+
+//@ functions
+
+// startNewGame : this function will reset the game score and squares and will inform the player to start by printing a massage that states that.
+
+// randomSquare : this function will generate a random square from the array.
+
+// addSquare : this function will add a random square
+
+// display : this function will show the sequence to the player (flashing)
+
+// getPlayerInput : this function will receive the player input
+
+// checkPlayerInput : this function will check if the player input matches the flashing sequence by looping through the players input and see if it matches, if it dosent match it will go to game over
+
+// checkGameOver : this function will handle if the game is over by checking if the player has chosen the correct squares in the order specified and will display a game over message.
+
+//
